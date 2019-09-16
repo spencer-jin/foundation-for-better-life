5fedf6d7a341b5fc34042dbcb0d6300102f06e9e
John Paul Dillard <johnpauldillard@John-Pauls-MacBook-Air-9.local>
2019-09-16 12:25:05 -0700

    added includes and preprocessor directives

---
+++ Wrench.cpp
@@ -0,0 +1,5 @@
+//
+// Created by John Paul Dillard on 9/16/19.
+//
+
+#include "Wrench.h"
---
+++ Wrench.h
@@ -0,0 +1,14 @@
+//
+// Created by John Paul Dillard on 9/16/19.
+//
+
+#ifndef FOUNDATION_FOR_BETTER_LIFE_WRENCH_H
+#define FOUNDATION_FOR_BETTER_LIFE_WRENCH_H
+
+
+class Wrench {
+
+};
+
+
+#endif //FOUNDATION_FOR_BETTER_LIFE_WRENCH_H
