5274db4c4eeb26857d414d0df1ef0d9980085fb2
Anthony Khoshrozeh <akhoshrozeh@gmail.com>
2019-09-16 15:00:34 -0700

    Player class definition and implementation

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,53 @@
 #include "Player.h"
+
+Player::Player()
+{
+    m_health = 25;
+    m_gas = 25;
+    m_drunkState = false;
+    m_isDead = false;
+}
+
+int Player::getHealth()
+{
+    return m_health;
+}
+
+int Player::getGas()
+{
+    return m_gas;
+}
+
+bool Player::isDrunk()         // returns True when player is drunk
+{
+    return m_drunkState;
+}
+
+bool Player::isDead()
+{
+    if(m_gas <= 0 || m_health <= 0)
+        m_isDead = true;
+    
+    return m_isDead;
+}
+
+void Player::setHealth(int h)
+{
+    m_health = h;
+    
+    if(m_health <= 0)
+        m_isDead = true;
+}
+
+void Player::setGas(int g)
+{
+    m_gas = g;
+    
+    if(m_gas <= 0)
+        m_isDead = true;
+}
+
+void Player::setDrunkState(bool d)
+{
+    m_drunkState = d;
+}
--- Player.h
+++ Player.h
@@ -1,4 +1,29 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+class Player
+{
+public:
+    Player();
+    int getHealth();
+    int getGas();
+    bool isDrunk();         // returns True when player is drunk
+    bool isDead();          // checks m_gas and m_health before returning
+    
+    void setHealth(int health);     // checks to see if player is dead as well
+    void setGas(int gas);           // checks to see if player is dead as well
+    void setDrunkState(bool drunk);
+    
+    
+    
+private:
+    int m_gas;
+    int m_health;
+    bool m_drunkState;      // is True when player is drunk
+    bool m_isDead;            // returns True if m_health or m_gas is 0
+    
+    
+};
+
+
 #endif // PLAYER_H
