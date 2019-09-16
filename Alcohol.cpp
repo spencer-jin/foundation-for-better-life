ff0d73485830e17818e19c304e1ea61d5fc7fda7
Nathakin Leepreecha <iamnathan13579@gmail.com>
2019-09-16 15:59:36 -0700

    solved merge problem

--- Alcohol.cpp
+++ Alcohol.cpp
@@ -1,11 +1,7 @@
 #include "Alcohol.h"
 #include "Player.h"
 
-<<<<<<< HEAD
+
 void Alcohol::influencePlayer(Player &player) {
-    player.setDrunkState(true);
-=======
-void Alcohol::influencePlayer(Player &p) {
-	p.setDrunkState(true);// sets player's drunk status to true
->>>>>>> 9b2170f20aaf90f80f3efe631c30cfd7a58d1275
+	player.setDrunkState(true);
 }
--- Alcohol.h
+++ Alcohol.h
@@ -8,9 +8,4 @@ public:
     virtual void influencePlayer(Player &player); //sets player status as drunk
 }
 
-class Alcohol : public GameObject {
-public:
-	void influencePlayer(Player&);
-};
-
 #endif // ALCOHOL_H
